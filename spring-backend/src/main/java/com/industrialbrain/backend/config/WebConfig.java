b590840ded9275a6222eaae530ae83575ee7ee96
KADIRI SAI KUMAR <saikumarkadirik@gmail.com>
2026-07-18 16:35:54 +0530

    Web  configuration

--- spring-backend/src/main/java/com/industrialbrain/backend/config/WebConfig.java
+++ spring-backend/src/main/java/com/industrialbrain/backend/config/WebConfig.java
@@ -1,11 +1,23 @@
 package com.industrialbrain.backend.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(false);
+    }
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
