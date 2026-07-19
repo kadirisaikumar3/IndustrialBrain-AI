2af18ad5f348f3afe1a60a90316c90576f0883ef
KADIRI SAI KUMAR <saikumarkadirik@gmail.com>
2026-07-19 19:24:42 +0530

    Configure multipart upload limits explicitly

--- spring-backend/src/main/java/com/industrialbrain/backend/config/MultipartConfig.java
+++ spring-backend/src/main/java/com/industrialbrain/backend/config/MultipartConfig.java
@@ -1,7 +1,7 @@
 package com.industrialbrain.backend.config;
 
 import jakarta.servlet.MultipartConfigElement;
-import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.boot.servlet.MultipartConfigFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.util.unit.DataSize;
